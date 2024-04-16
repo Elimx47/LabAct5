910549e8d8d5493e6aeca416214ce3acaf99011f
Elimx47 <znyrpvillanueva@gmail.com>
2024-04-16 14:55:56 +0800

    Added Percentage

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,9 @@
 function Footer({ items }) {
   let completedItems = items.filter((item) => item.isChecked).length;
   const itemText = items.length >= 2 ? "items" : "item";
+  const percentageCompleted =
+    items.length > 0 ? ((completedItems / items.length) * 100).toFixed(2) : 0;
+
   return (
     <div
       style={{
@@ -17,6 +20,7 @@ function Footer({ items }) {
         You have {items.length} {itemText} in your list, and you already
         completed {completedItems}
       </p>
+      <p>({percentageCompleted}%)</p>
     </div>
   );
 }
